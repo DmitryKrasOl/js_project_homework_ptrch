3fad49ef917557fa7eb5e7d9e4952e67fbb71185
Dima <dikrasnikov@gmail.com>
2023-08-27 22:11:33 +0300

    Lesson 12 Homework. Cycles

--- js/script.js
+++ js/script.js
@@ -3,19 +3,53 @@
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
 
 const personalVovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
 };
 
 let a, b;
 
+a = +"dsdfdsf"
+console.log(a);
+
 for (let i = 0; i < 2; i++) {
-  a = prompt("Один из последних просмотренных фильмов?", "");
-  b = +prompt("На сколько оцените его?", "");
-  personalVovieDB.movies[a] = b;
+   let toggle = true;
+   do {
+      a = prompt("Один из последних просмотренных фильмов?", "");
+      // console.log(a.length);
+      if (a.length === 0) {
+         alert("Всетаки ответьте на вопрос");
+         toggle = false;
+      } else if (a.length >= 50) {
+         console.log(a.length);
+         alert("Введите не больше 50-и символов");
+         toggle = false;
+      } else {
+         toggle = true;
+      }
+   } while (toggle === false);
+
+   do {
+      b = +prompt("На сколько оцените его?", "");
+      if (b.length === 0) {
+         alert("Введите оценку.");
+         toggle = false;
+      } else if (isNaN(b)) {
+         alert("Введите число");
+         toggle = false;
+      } else {
+         toggle = true;
+      }
+   } while (toggle === false);
+
+   personalVovieDB.movies[a] = b;
 }
 
+
+// let a = "";
+// let g = a.length;
+// console.log(`g = ${g}`);
 console.log(personalVovieDB);
