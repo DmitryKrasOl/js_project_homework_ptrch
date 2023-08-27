ac6aa1ff8cd6e930a58bc5a28c6e42a20699446c
Dima <dikrasnikov@gmail.com>
2023-08-27 13:40:21 +0300

    lesson_9

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 "use strict";
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели", "");
+
+const personalVovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let a, b;
+
+for (let i = 0; i < 2; i++) {
+  a = prompt("Один из последних просмотренных фильмов?", "");
+  b = +prompt("На сколько оцените его?", "");
+  personalVovieDB.movies[a] = b;
+}
+
+console.log(personalVovieDB);
